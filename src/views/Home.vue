84ec43fe55d1a611f2f112cf6287935bbfdf5325
tomcatmwi <tamas.polgar.pixeldog@gmail.com>
2022-12-08 23:33:13 +0100

    fix: single component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
+<template src="./Home.html" />
+
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 
 @Options({
+  name: "Home",
   components: {},
-  template: "./Home.html",
 })
 export default class Home extends Vue {}
 </script>
