aa6d9033bb3157efa683a8418bec74e1849cb603
tomcatmwi <tamas.polgar.pixeldog@gmail.com>
2022-12-08 18:30:39 +0100

    fix: add .vscode

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {},
+  template: "./Home.html",
+})
+export default class Home extends Vue {}
+</script>
